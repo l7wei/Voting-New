42c999b0a2c2a92180bfc3f34718a780500f02b4
Wei <kiwi072400.lin@gmail.com>
2025-11-20 00:40:43 +0800

    Add favicon

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,6 +4,9 @@ import "./globals.css";
 export const metadata: Metadata = {
   title: "學生會投票系統",
   description: "National Tsing Hua University Voting System",
+  icons: {
+    icon: "/favicon.png",
+  },
 };
 
 export default function RootLayout({
